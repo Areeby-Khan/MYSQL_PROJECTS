4786540d0906f6ef26ad32964039c8a359ead557
Areeby khan <areebalouch@gmail.com>
2024-11-28 17:44:09 +0500

    Normalization

---
+++ Normalization/FirstNormalFormPractice_01.sql
@@ -0,0 +1,4 @@
+--Normalization TOPIC
+
+--Normalization is a process in database design that organizes data to minimize redundancy and dependency. The goal of normalization is to divide a database into smaller, more manageable tables while ensuring data integrity and eliminating unnecessary duplication. It helps improve database efficiency, consistency, and maintainability.
+
