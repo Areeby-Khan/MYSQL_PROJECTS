d79079661ad053794ed593078fc0cd8ddf6b035b
Areeby khan <areebalouch@gmail.com>
2024-11-30 16:45:39 +0500

    Practice_01 (Keys)

--- Normalization/FirstNormalFormPractice_01.sql
+++ Normalization/FirstNormalFormPractice_01.sql
@@ -1,4 +1,20 @@
 --Normalization TOPIC
 
 --Normalization is a process in database design that organizes data to minimize redundancy and dependency. The goal of normalization is to divide a database into smaller, more manageable tables while ensuring data integrity and eliminating unnecessary duplication. It helps improve database efficiency, consistency, and maintainability.
+------------------------------------------------------------
 
+--PRIMARY KEY
+--A unique identifier for each row in a table.
+--It must be unique and cannot be NULL.
+
+------------------------------------------------------------
+
+--COMPOSITE KEY
+--A key made up of two or more columns that together uniquely identify a row in a table.
+--It’s used when one column alone cannot uniquely identify a row.
+
+------------------------------------------------------------
+
+--SUPER KEY
+-- Any combination of columns that can uniquely identify a row in a table.
+-- A primary key is the simplest form of a super key. Super keys can include extra columns.
